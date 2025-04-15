54a59021699e9338f88876940ae619b725c28270
Zhengyu (Eddy) <75236857+ZhengyuEddy@users.noreply.github.com>
2025-04-15 05:22:53 +0000

    Modify the template to my details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Zhengyu (Eddy) Yang",
+  title: "PhD Candidate, MPH, MBBS",
+  email: "eddy.yang@monash.edu",
+  gitHub: "ZhengyuEddy",
+  linkedIn: "Zhengyu (Eddy) Yang",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
